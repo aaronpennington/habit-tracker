b81cd54c1bdf5fe0d2dd84cb6b6ad3ac88ba9e75
Aaron Pennington <b.aaron.pennington@gmail.com>
2026-08-11 20:48:02 -0500

    Add cmake-format build target

--- cmake/format.cmake
+++ cmake/format.cmake
@@ -1,21 +1,42 @@
-# Format all files using clang-format
-
+# Format all c++ files using clang-format
 find_program(CLANG_FORMAT_EXE NAMES clang-format)
 
 if(CLANG_FORMAT_EXE)
   message(STATUS "Found clang-format: ${CLANG_FORMAT_EXE}")
 
-  file(GLOB_RECURSE SOURCES
-    "${CMAKE_SOURCE_DIR}/src/*.cpp"
-    "${CMAKE_SOURCE_DIR}/src/*.hpp"
-    "${CMAKE_SOURCE_DIR}/include/*.h"
-  )
+  file(GLOB_RECURSE _CPP_FILES "${CMAKE_SOURCE_DIR}/src/*.cpp"
+       "${CMAKE_SOURCE_DIR}/src/*.hpp" "${CMAKE_SOURCE_DIR}/include/*.h")
 
-  add_custom_target(format
-      COMMAND ${CLANG_FORMAT_EXE} -i -style=file ${SOURCES}
-      COMMENT "Formatting..."
-      WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-  )
+  add_custom_target(
+    clangformat
+    COMMAND ${CLANG_FORMAT_EXE} -i -style=file ${_CPP_FILES}
+    COMMENT "Formatting cpp files..."
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
 else()
   message(WARNING "clang-format executable not found.")
 endif()
+
+# Format all cmake files using cmake-format
+if(WIN32)
+  set(CMAKE_FORMAT_EXE "${CMAKE_SOURCE_DIR}/.venv/Scripts/cmake-format.exe")
+else()
+  set(CMAKE_FORMAT_EXE "${CMAKE_SOURCE_DIR}/.venv/bin/cmake-format")
+endif()
+
+if(CMAKE_FORMAT_EXE)
+  message(STATUS "Found cmake-format: ${CMAKE_FORMAT_EXE}")
+
+  file(GLOB_RECURSE _CMAKE_FILES "${CMAKE_SOURCE_DIR}/CMakeLists.txt"
+       "${CMAKE_SOURCE_DIR}/*.cmake")
+
+  add_custom_target(
+    cmakeformat
+    COMMAND ${CMAKE_FORMAT_EXE} -i ${_CMAKE_FILES}
+    COMMENT "Formatting cmake files..."
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+else()
+  message(WARNING "cmake-format executable not found.")
+endif()
+
+add_custom_target(format COMMAND ${CMAKE_COMMAND} -E echo "Formatting...")
+add_dependencies(format clangformat cmakeformat)
